fe613d84b3cf9d5784facab9b69bdaaa328f9a46
gazdagergo <info@gazdagergo.com>
2018-09-18 09:18:05 +0200

    ES6 simplifications

--- src/react-index.js
+++ src/react-index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./styles.css";
 
-const App = function(props) {
-  return <span>{props.content}</span>;
-};
+const App = ({ content }) => <span>{content}</span>;
 
 const reactRoot = document.getElementById("react-root");
 ReactDom.render(<App content="hello react" />, reactRoot);
