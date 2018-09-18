663a1b0edbd18113b57cbfdaf45f2306c681c954
gazdagergo <info@gazdagergo.com>
2018-09-18 09:24:38 +0200

    children

--- src/react-index.js
+++ src/react-index.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./styles.css";
 
-const App = ({ content }) => <span>{content}</span>;
+const Colorize = ({ children, color }) => {
+  return <span style={{ color }}>children</span>;
+};
+
+const App = ({ content }) => {
+  return (
+    <h1>
+      <Colorize color="red">
+        <span>{content}</span>
+      </Colorize>
+    </h1>
+  );
+};
 
 const reactRoot = document.getElementById("react-root");
 ReactDom.render(<App content="hello react" />, reactRoot);
