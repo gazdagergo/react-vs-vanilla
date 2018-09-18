d1b8d61f41e4d2eff773717c2ca8b2431bb5cba3
gazdagergo <info@gazdagergo.com>
2018-09-18 09:04:54 +0200

    iife

--- src/js-index.js
+++ src/js-index.js
@@ -1,9 +1,11 @@
-function jsApp(content, root) {
-  const span = document.createElement("SPAN");
-  const text = document.createTextNode(content);
-  span.appendChild(text);
-  root.appendChild(span);
-}
+(function() {
+  function jsApp(content, root) {
+    const span = document.createElement("SPAN");
+    const text = document.createTextNode(content);
+    span.appendChild(text);
+    root.appendChild(span);
+  }
 
-const jsRootElement = document.getElementById("js-root");
-jsApp("hello js", jsRootElement);
+  const jsRootElement = document.getElementById("js-root");
+  jsApp("hello js", jsRootElement);
+})();
